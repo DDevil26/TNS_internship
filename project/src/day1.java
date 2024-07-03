cc3ea4995ef3bae5ad650e1d3fdb054ba2ca7ee6
SMIT <126007353+DDevil26@users.noreply.github.com>
2024-07-03 21:31:34 +0530

    Add files via upload

---
+++ project/src/day1.java
@@ -0,0 +1,27 @@
+public class day1
+{
+    public static void main(String[] args)
+    {
+        int res = 50;
+        if(res>=90)
+        {
+            System.out.println("A");
+        }
+        else if (res>=80) 
+        {
+            System.out.println("B");  
+        }
+        else if (res>=70) 
+        {
+            System.out.println("C");    
+        }
+        else if (res>=60) 
+        {
+            System.out.println("D");    
+        }
+        else
+        {
+            System.out.println("FAIL");
+        }
+    }
+}
